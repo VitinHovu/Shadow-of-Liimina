1103dea8180c1f9f488f1d160e89b756af49d3f2
Hector Parada <hectorparada604@gmail.com>
2021-11-03 20:44:06 -0400

    Movement with Animations
    Animations still needs some work to make walking fluent

--- Mythos-Shadow of Liimina/Assets/Scripts/PlayerMovement.cs
+++ Mythos-Shadow of Liimina/Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,11 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D playerRigidbody;
     private Vector3 change;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -20,13 +22,27 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
 
-        //if there is an actual change happing
-        if(change != Vector3.zero)
-        {
-            MoveCharacter();
-        }
         
+        
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        //if there is an actual change happing
+        if (change != Vector3.zero)
+        {
+
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
     }
 
     ///Call Rigidbody and set to move towards the position of the current position + change 
